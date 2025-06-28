3f7c70e1d1d92dcca447889bb510494e19994b1f
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-06-28 15:24:45 +0100

    creatte the base model/domain application entry

---
+++ base-model/src/main/java/com/adagedo_softengineer/BaseModelApplication.java
@@ -0,0 +1,11 @@
+package com.adagedo_softengineer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SprintBootApplication
+public class BaseModelApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(BaseModelApplication.class, args);
+    }
+}

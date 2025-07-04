64a1265190f5d9f077f86400cf5217c910b061d6
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-07-04 15:43:54 +0100

    order consumer recieved setups

--- stock-service/src/main/java/com/adagedo_softengineer/kafka/OrderConsumer.java
+++ stock-service/src/main/java/com/adagedo_softengineer/kafka/OrderConsumer.java
@@ -2,6 +2,8 @@ package com.adagedo_softengineer.kafka;
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import com.adagedo_softengineer.dto.OrderEvent;
 
 
 
@@ -10,8 +12,15 @@ public class OrderConsumer {
 
     private static final Logger logger = LoggerFactory.getLogger(OrderConsumer.class);
 
+    @KafkaListener(
+            topics = "${spring.kafka.topic.name}", 
+            groupId = "${spring.kafka.consumer.group-id}"
+    )
     public void consume(OrderEvent event) {
         
+        logger.info(String.format("order recieved in the stock service => %s", event.toString()));
+
+        // you can save the order event data into the database
     }
 
 }

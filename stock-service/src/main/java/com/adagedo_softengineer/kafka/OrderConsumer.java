3080d8a5dc66a71084c9ad77462050752b480a70
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-07-04 15:30:48 +0100

    order consumer setups

--- stock-service/src/main/java/com/adagedo_softengineer/kafka/OrderConsumer.java
+++ stock-service/src/main/java/com/adagedo_softengineer/kafka/OrderConsumer.java
@@ -1,5 +1,17 @@
 package com.adagedo_softengineer.kafka;
+import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+
+
+@Service
 public class OrderConsumer {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderConsumer.class);
+
+    public void consume(OrderEvent event) {
+        
+    }
 
 }
